180e951dc4701f26d85ced0bb1cbe43e924c4412
Luis A. Vargas <luisvargascr@gmail.com>
2017-10-20 19:57:05 -0700

    10/20/2017 - Added Comments for Insert Sort Algorithm.

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,4 +1,5 @@
-﻿using Algorithms.SortingAlgorithms.Selection;
+﻿using Algorithms.SortingAlgorithms.Insertion;
+using Algorithms.SortingAlgorithms.Selection;
 using System;
 
 namespace Algorithms
@@ -7,10 +8,22 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            var Selection = new SelectionSort();
-            var SortedArray = Selection.Sort(new int[] { 7, 8, 5, 4, 9, 2 });
+            int[] UnsortedArray1 = new int[] { 7, 8, 5, 4, 9, 2 };
+            int[] UnsortedArray2 = new int[] { 7, 8, 5, 4, 9, 2 };
 
-            foreach (int num in SortedArray)
+            var Selection = new SelectionSort();
+            var SortedArray1 = Selection.Sort(UnsortedArray1);
+            var Insertion = new InsertionSort();
+            var SortedArray2 = Insertion.Sort(UnsortedArray2);
+
+            Console.WriteLine("Selection Sort Algorithm:");
+            foreach (int num in SortedArray1)
+                Console.WriteLine(num.ToString());
+
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine("Insertion Sort Algorithm:");
+            foreach (int num in SortedArray2)
                 Console.WriteLine(num.ToString());
 
             Console.ReadLine();
---
+++ Algorithms/SortingAlgorithms/Insertion/InsertionSort.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.SortingAlgorithms.Insertion
+{
+    public class InsertionSort
+    {
+        public int[] Sort (int[] list)
+        {
+            int i, j, key, temp = 0;
+
+            for (i = 1; i < list.Length; i++)
+            {
+                Console.WriteLine("--------------------------------------");
+                key = list[i];
+                j = i - 1;
+                Console.WriteLine(string.Format("i = {0}; j = {1}", i, j));
+                PrintCurrentStatusOfArray(list,i,j);
+                Console.WriteLine(string.Format("key {0} < j {1}", key, list[j]));
+  
+                while (j >= 0 && key < list[j])
+                {
+                    temp = list[j];
+                    
+                    list[j] = list[j + 1];
+                    
+                    list[j + 1] = temp;
+                    Console.WriteLine(string.Format("Swap A[{0}] -->A[{1}] ", j, j + 1));
+                    j--;
+                }
+            }
+            return list;
+        }
+
+        private void PrintCurrentStatusOfArray(int[] list, int i, int j)
+        {
+            var List = string.Empty;
+            int cnt1 = 0;
+            int cnt2 = 0;
+
+            foreach (int SingleNum in list)
+            {
+                if (cnt1 == i)
+                {
+                    List += "(" + SingleNum + ")";
+                }
+                else if (cnt2 == j)
+                {
+                    List += "[" + SingleNum + "]";
+                }
+                else
+                {
+                    List += SingleNum + ",";
+                }
+                List = List.TrimEnd();
+                cnt1 += 1;
+                cnt2 += 1;
+            }
+            Console.WriteLine(string.Format("A = {0}", List));
+        }
+    }
+}
